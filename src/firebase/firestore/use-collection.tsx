7693ac6984004a21593cf9a49e9e8547244a7e63
TUSHAR SINHA <tsinha863@gmail.com>
2026-01-18 08:41:09 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -58,7 +58,7 @@ export function useCollection<T = any>(
   type StateDataType = ResultItemType[] | null;
 
   const [data, setData] = useState<StateDataType>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<FirestoreError | Error | null>(null);
 
   useEffect(() => {
@@ -71,8 +71,8 @@ export function useCollection<T = any>(
 
     setIsLoading(true);
     setError(null);
+    setData(null); // Clear previous data
 
-    // Directly use memoizedTargetRefOrQuery as it's assumed to be the final query
     const unsubscribe = onSnapshot(
       memoizedTargetRefOrQuery,
       (snapshot: QuerySnapshot<DocumentData>) => {
