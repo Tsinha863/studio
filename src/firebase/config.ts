909400e926bf620acc0a8a24f2900c4c1a51b614
TUSHAR SINHA <tsinha863@gmail.com>
2026-01-22 21:37:39 +0000

    STEP 2 — APPLY THESE 5 GUARANTEED FIXES (90% of cases) 🔴 FIX 1: Missing

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5076448380-e55b6",
-  "appId": "1:760319461640:web:983a6e372197e5ee1544bd",
-  "apiKey": "AIzaSyD8OotxFK1y1tDDWSnGoBDTfcIjbQkXdMY",
-  "authDomain": "studio-5076448380-e55b6.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "760319461640"
-};
